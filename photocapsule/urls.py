ec7b36b7a1f96f9cd57230317eccde0c4958cb9e
“Liam <2759113M@student.gla.ac.uk>
2024-03-15 21:17:19 +0000

    Browse and Results

--- photocapsule/urls.py
+++ photocapsule/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('upload', views.upload, name='upload'),
     path('browse', views.browse, name='browse'),
-    path('browse/username', views.profileResults, name='profile-results'),
-    path('browse/category', views.categoryResults, name='category-results'),
+    path('browse/profile/<str:userPage>', views.profileResults, name='profile-results'),
+    path('browse/category/<str:category>', views.categoryResults, name='category-results'),
     path('profile/<str:userPage>', views.profile, name='profile'),
     path('profile/<str:userPage>/edit', views.editProfile, name='edit-profile'),
-    path('photos/username/image-id', views.photo, name='photo'),
+    path('photos/<str:userPage>/<str:photo>', views.photo, name='photo'),
 ]
