c33bd0ebb43d558cf868620503fa5182025085c8
Liam Murphy <2759113m@student.gla.ac.uk>
2024-03-17 23:38:03 +0000

    Expanded Population Script and Removed Unwanted Files

--- photocapsule/models.py
+++ photocapsule/models.py
@@ -1,21 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profilePicture = models.ImageField(upload_to='profile_pictures', default='profile_pictures/default.png') 
     def __str__(self):
         return self.user.username
-    
 
 class Category(models.Model):
     categoryName=models.CharField(max_length=25, unique=True)
     
     def __str__(self):
         return self.categoryName
-
 
 class Photo(models.Model):
     userID = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -30,16 +26,10 @@ class Photo(models.Model):
 class UserLike(models.Model):
     userID = models.ForeignKey(User, on_delete=models.CASCADE)
     photoID = models.ForeignKey(Photo, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
-    
-    
-    
     
 class CategoryPhoto(models.Model):
     photoID = models.ForeignKey(Photo, on_delete=models.CASCADE)
     categoryID = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-
     
 class Comment(models.Model):
     photoID = models.ForeignKey(Photo, on_delete=models.CASCADE)
