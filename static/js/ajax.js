e3ce82ff3dd7b4c6851fc82d58dd8bd503350dc0
Liam Murphy <2759113m@student.gla.ac.uk>
2024-03-18 17:31:59 +0000

    Profile Search now Dynamically Updates using AJAX

---
+++ static/js/ajax.js
@@ -0,0 +1,14 @@
+function getProfiles() {
+    var xhttp = new	XMLHttpRequest();	
+    xhttp.onreadystatechange = function() {	
+        if (this.readyState	== 4 &&	this.status	== 200) {	
+            document.getElementById("profile-list").innerHTML = this.responseText;
+        }
+    };
+    var search = document.getElementById("search").value;
+    xhttp.open("POST","",true);
+    xhttp.setRequestHeader("X-CSRFToken", csrftoken);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+    xhttp.send("profile="+search);
+}
